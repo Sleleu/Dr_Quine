dd3901c3134a3a0ca190a60eb71ef1fb50ba23f8
sleleu <leleu.sebastien@hotmail.fr>
2023-06-28 00:36:10 +0200

    add Grace.py

--- PYTHON/Grace.py
+++ PYTHON/Grace.py
@@ -1,1 +1,5 @@
+# A comment
+
 NAME = "Grace_kid.py"
+CODE = "# A comment%c%cNAME = %cGrace_kid.py%c%cCODE = %c%s%c%cGRACE = f = open(NAME, %cw%c); f.write(CODE %%(10,10,34,34,10,34,CODE,34,10,34,34)); f.close()"
+GRACE = f = open(NAME, "w"); f.write(CODE %(10,10,34,34,10,34,CODE,34,10,34,34)); f.close()
