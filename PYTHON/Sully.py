0c9b7bc5f61dca5ed66780e7d3543546fdc40018
sleleu <leleu.sebastien@hotmail.fr>
2023-06-28 02:08:49 +0200

    fix indentation

--- PYTHON/Sully.py
+++ PYTHON/Sully.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
         exit(0)
 
     name = f"Sully_{x - 1}.py"
-    code = "if __name__ == %c__main__%c:%c%cx = %d%c%cif (x <= 0):%c%c%cexit(0)%c%c%cname = f%cSully_{x - 1}.py%c%c%ccode = %c%s%c%c%cwith open (name, %cw%c) as f:%c%c%cf.write(code %%(34,34,10,9,x - 1,10,9,10,9,9,10,10,9,34,34,10,9,34,code,34,10,9,34,34,10,9,9,10,9,34,34))%c%cexec(open(f%cSully_{x - 1}.py%c).read())"
+    code = "if __name__ == %c__main__%c:%c    x = %d%c    if (x <= 0):%c        exit(0)%c%c    name = f%cSully_{x - 1}.py%c%c    code = %c%s%c%c    with open (name, %cw%c) as f:%c        f.write(code %%(34,34,10,x - 1,10,10,10,10,34,34,10,34,code,34,10,34,34,10,10,34,34))%c    exec(open(f%cSully_{x - 1}.py%c).read())"
     with open (name, "w") as f:
-        f.write(code %(34,34,10,9,x - 1,10,9,10,9,9,10,10,9,34,34,10,9,34,code,34,10,9,34,34,10,9,9,10,9,34,34))
+        f.write(code %(34,34,10,x - 1,10,10,10,10,34,34,10,34,code,34,10,34,34,10,10,34,34))
     exec(open(f"Sully_{x - 1}.py").read())
