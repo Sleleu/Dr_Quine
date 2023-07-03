d90fa6fef7acdbb8c3be8ea1717eeaf3946b6593
sleleu <leleu.sebastien@hotmail.fr>
2023-07-03 04:43:44 +0200

    continue Sully

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -10,14 +10,16 @@ global main
 section .data
 	name: db "Sully_%d.s", 0
 	asm_cmd: db "nasm -f elf64 Sully_%d.s", 0
-	compile_cmd: db "gcc -Wextra -Wall -Werror -no-pie Sully_%d.o -o Sully_%d", 0
+	link_cmd: db "gcc -Wextra -Wall -Werror -no-pie Sully_%d.o -o Sully_%d", 0
 	exec_cmd: db "./Sully_%d", 0
 	x: equ 5
+	code : db "bits 64%1$c%1$c", 0
 
     write_mode: db "w", 0
 
 section .bss
-	buff_compile: resb 80
+	buff_asm: resb 50
+	buff_link: resb 80
 	buff_execute: resb 50
 	buff_name: resb 50
 
@@ -31,12 +33,26 @@ main:
 	cmp rax, 0
 	jle return
 
-
 	;snprintf name
 	mov rdi, buff_name
 	mov rsi, 50
 	mov rdx, name
 	mov rcx, x - 1
+	call snprintf
+
+	;snprintf assemble
+	mov rdi, buff_asm
+	mov rsi, 50
+	mov rdx, asm_cmd
+	mov rcx, x - 1
+	call snprintf
+
+	;snprintf link
+	mov rdi, buff_link
+	mov rsi, 80
+	mov rdx, link_cmd
+	mov rcx, x - 1
+	mov r8, x - 1
 	call snprintf
 
 	; open
@@ -46,12 +62,23 @@ main:
     cmp rax, 0 ; check error
     je ret_error
 
-   	mov rbx, rax ; if no error while open
+	;fprintf
+	mov rbx, rax ; if no error, store addr in rbx
+   	mov rdi, rbx
+	mov rsi, code
+	mov rdx, 10
+	call fprintf
+
+	;close
+    mov rdi, rbx
+    call fclose
 
 exec:
-	mov rdi, compile_cmd
+	mov rdi, buff_asm
 	call system
-	mov rdi, exec_cmd
+	mov rdi, buff_link
+	call system
+	mov rdi, buff_execute
 	call system
 
 return:
