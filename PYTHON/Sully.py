491a16d2a0105b600c2ce2a953b2a4705d94e0f9
sleleu <leleu.sebastien@hotmail.fr>
2023-07-06 15:54:54 +0200

    fix bug sully

--- PYTHON/Sully.py
+++ PYTHON/Sully.py
@@ -1,10 +1,13 @@
+import os
 if __name__ == "__main__":
     x = 5
     if (x <= 0):
         exit(0)
-
-    name = f"Sully_{x - 1}.py"
-    code = "if __name__ == %c__main__%c:%c    x = %d%c    if (x <= 0):%c        exit(0)%c%c    name = f%cSully_{x - 1}.py%c%c    code = %c%s%c%c    with open (name, %cw%c) as f:%c        f.write(code %%(34,34,10,x - 1,10,10,10,10,34,34,10,34,code,34,10,34,34,10,10,34,34))%c    exec(open(f%cSully_{x - 1}.py%c).read())"
+    file_name = os.path.basename(__file__)
+    if "Sully_" in file_name :
+        x -= 1
+    name = f"Sully_{x}.py"
+    code = "import os%cif __name__ == %c__main__%c:%c    x = %d%c    if (x <= 0):%c        exit(0)%c    file_name = os.path.basename(__file__)%c    if %cSully_%c in file_name :%c        x -= 1%c    name = f%cSully_{x}.py%c%c    code = %c%s%c%c    with open (name, %cw%c) as f:%c        f.write(code %%(10,34,34,10,x,10,10,10,10,34,34,10,10,34,34,10,34,code,34,10,34,34,10,10,34,34,34,34,34,34))%c    os.system(%cpython3 %c + %cSully_%c + str(x) + %c.py%c)"
     with open (name, "w") as f:
-        f.write(code %(34,34,10,x - 1,10,10,10,10,34,34,10,34,code,34,10,34,34,10,10,34,34))
-    exec(open(f"Sully_{x - 1}.py").read())
+        f.write(code %(10,34,34,10,x,10,10,10,10,34,34,10,10,34,34,10,34,code,34,10,34,34,10,10,34,34,34,34,34,34))
+    os.system("python3 " + "Sully_" + str(x) + ".py")
